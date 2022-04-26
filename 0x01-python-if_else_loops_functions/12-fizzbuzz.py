4efb9df97e45f69c577909466df17a7c7a0d617b
adut24 <dutertre.alexandre@laposte.net>
2022-04-26 13:29:28 +0200

    Print the Fizz-Buzz test

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,6 +7,6 @@ def fizzbuzz():
             if i % 5 == 0:
                 print("Buzz", end="")
         else:
-            print("{:d}".format(i), end=" ")
+            print("{:d}".format(i), end="")
         if i != 100:
             print(" ", end="")
