3cf5e4e962bb1a3908945f47dfc316d02ca31ea6
adut24 <dutertre.alexandre@laposte.net>
2022-05-02 13:33:24 +0200

    Small correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,13 +2,13 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
-            return (0 + tuple_b[0], 0 + tuple_b[1])
+            return tuple_b
         elif len(tuple_a) == 1:
-            return (tuple_b[0] + tuple_b[0], 0 + tuple_b[1])
+            return (tuple_a[0] + tuple_b[0], tuple_b[1])
     elif len(tuple_b) < 2:
         if len(tuple_b) == 0:
-            return (tuple_a[0] + 0, tuple_a[1] + 0)
+            return tuple_a
         elif len(tuple_b) == 1:
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+            return (tuple_a[0] + tuple_b[0], tuple_a[1])
     else:
         return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
