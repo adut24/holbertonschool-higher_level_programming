a652b4a4c8109be22ed9d3abbe733ada5b0e6f3a
adut24 <dutertre.alexandre@laposte.net>
2022-05-02 14:16:44 +0200

    Correction in case the list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
-
     if my_list is None:
         return None
-
+    if my_list == []:
+        return None
+    max = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
