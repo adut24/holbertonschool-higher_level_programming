d99c3c60f7f6eabc3438ca6c93510e2ca60b93d4
adut24 <dutertre.alexandre@laposte.net>
2022-05-02 12:05:14 +0200

    Correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([' '.join(['{}'.format(n) for n in row])
-     for row in matrix]))
+    a = matrix
+    print('\n'.join([' '.join(['{}'.format(n) for n in row]) for row in a]))
