d8c124707713c63e81260d57a973d11553b8f8bd
adut24 <dutertre.alexandre@laposte.net>
2022-05-04 15:13:04 +0200

    Return a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    len1 = len(set_1)
+    len2 = len(set_2)
+    new_set = set({})
+    item1 = next(iter(set_1))
+    item2 = next(iter(set_2))
+
+    if len1 < len2:
+        for i in range(len1):
+            if item1 == item2:
+                new_set.add(item1)
+            item1 = next(iter(set_1))
+            item2 = next(iter(set_2))
+    else:
+        for i in range(len2):
+            if item1 == item2:
+                new_set.add(item1)
+            item1 = next(iter(set_1))
+            item2 = next(iter(set_2))
+    return new_set
