0fc6110a1f376100764e60918296d31577e65cc7
adut24 <dutertre.alexandre@laposte.net>
2022-05-04 15:47:52 +0200

    Simplification of the code

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_set = set({})
-    it1 = iter(set_1)
-
-    for i in set_1:
-        item1 = next(it1)
-        it2 = iter(set_2)
-        for j in set_2:
-            item2 = next(it2)
-            if item1 == item2:
-                new_set.add(item1)
+    new_set = set_1 & set_2
     return new_set
