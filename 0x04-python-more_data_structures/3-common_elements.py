4ff956b7dd300e52a8b954b0bfbb1d99535e50a3
adut24 <dutertre.alexandre@laposte.net>
2022-05-04 15:43:47 +0200

    Correction for iteration

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    len1 = len(set_1)
-    len2 = len(set_2)
     new_set = set({})
-    item1 = next(iter(set_1))
-    item2 = next(iter(set_2))
+    it1 = iter(set_1)
 
-    if len1 < len2:
-        for i in range(len2):
+    for i in set_1:
+        item1 = next(it1)
+        it2 = iter(set_2)
+        for j in set_2:
+            item2 = next(it2)
             if item1 == item2:
                 new_set.add(item1)
-            item1 = next(iter(set_1))
-            item2 = next(iter(set_2))
-    else:
-        for i in range(len1):
-            if item1 == item2:
-                new_set.add(item1)
-            item1 = next(iter(set_1))
-            item2 = next(iter(set_2))
     return new_set
