ca042117599c918472618e718d4bbd5c4feceaab
adut24 <dutertre.alexandre@laposte.net>
2022-05-17 16:08:28 +0200

    Correction of condition

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,9 +75,9 @@ class Square:
         """
         change the position of the square
         """
-        if type(value) is not tuple and len(value) != 2 and\
-            type(value[0]) is not int and type(value[1]) is not int and\
-                value[0] < 0 and value[1] < 0:
+        if type(value) is not tuple or len(value) != 2 or\
+            type(value[0]) is not int or type(value[1]) is not int or\
+                value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
