eb8b3273226bb98caec6a7a12f9475e44a466b5e
adut24 <dutertre.alexandre@laposte.net>
2022-05-17 17:25:13 +0200

    Return the square as a string

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""
+module 6-square
+"""
+
+
+from re import S
+
+
+class Square:
+    """
+    class Square defining a square by size
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        method called when the instance is created
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+        if isinstance(position, tuple) and len(position) == 2 and\
+            isinstance(position[0], int) and isinstance(position[1], int) and\
+                position[0] >= 0 and position[1] >= 0:
+            self.__position = position
+        else:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+    def area(self):
+        """
+        return the area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        return the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        change the size of the square
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+        else:
+            raise TypeError('size must be an integer')
+
+    def __str__(self):
+        """
+        return the square
+        """
+        s = ''
+        if self.__size == 0:
+            return s
+        for a in range(self.__position[1]):
+            s += '\n'
+        for i in range(self.__size):
+            for p in range(self.__position[0]):
+                s += ' '
+            for j in range(self.__size):
+                s += '#'
+            s += '\n'
+        return s
+
+    @property
+    def position(self):
+        """
+        return the position of the square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        change the position of the square
+        """
+        if isinstance(value, tuple) and len(value) == 2 and\
+            isinstance(value[0], int) and isinstance(value[1], int) and\
+                value[0] >= 0 and value[1] >= 0:
+            self.__position = value
+        else:
+            raise TypeError('position must be a tuple of 2 positive integers')
