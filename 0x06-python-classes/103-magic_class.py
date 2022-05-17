cc9972b02ccd921b6d52c9cf0e76538df168c327
adut24 <dutertre.alexandre@laposte.net>
2022-05-17 18:14:37 +0200

    Give a specific bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module giving a specific bytecode"""
+
+
+import math
+
+
+class MagicClass:
+    """"Class giving a specific bytecode"""
+    def __init__(self, radius):
+        """method launched at the creation of the object"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """return the area of the circle"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """return the circumference of the circle"""
+        return 2 * math.pi * self.__radius
