c809cf4ee21ec512d9c0cee7e7355a8bd25d2fc1
adut24 <dutertre.alexandre@laposte.net>
2022-05-30 13:49:42 +0200

    Check if the object is an instance of, or inherited of

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_kind_of_class module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """tell if the object is an instance of the specified class"""
+    return isinstance(obj, a_class)
