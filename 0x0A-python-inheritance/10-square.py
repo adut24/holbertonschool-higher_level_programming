8bfec340ff794348d25406d5a5db599bcdcce000
adut24 <dutertre.alexandre@laposte.net>
2022-05-30 14:00:41 +0200

    Write a Square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def integer_validator(self, name, value):
+        """validate if it's an integer or not"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        """constructor method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """return an informal string"""
+        s = f'[Rectangle] {self.__width}/{self.__height}'
+        return s
+
+    def area(self):
+        """area method"""
+        return self.__height * self.__width
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        """constructor method"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
