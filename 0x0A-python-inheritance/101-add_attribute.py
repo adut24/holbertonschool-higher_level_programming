a6fea141a3f160413b4f94b7d56c3ce752446828
adut24 <dutertre.alexandre@laposte.net>
2022-05-30 18:36:47 +0200

    Correction for pycodestyle

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,5 +6,5 @@ def add_attribute(obj, key, value):
     """add a new attribute to an object if it's possible"""
     try:
         setattr(obj, key, value)
-    except:
+    except Exception:
         raise TypeError("can't add new attribute")
