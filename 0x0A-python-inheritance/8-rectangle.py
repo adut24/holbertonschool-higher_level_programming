472804b5483921b006be11dd016b473fb40ded48
adut24 <dutertre.alexandre@laposte.net>
2022-05-30 13:54:36 +0200

    Create a Rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """area method"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validate if it's an integer or not"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        """constructor method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
