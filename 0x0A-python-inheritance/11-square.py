8219736035d399551f734330e452fbf99534bb7e
adut24 <dutertre.alexandre@laposte.net>
2022-05-30 20:07:06 +0200

    Correction

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,34 +1,6 @@
 #!/usr/bin/python3
 """square module"""
-
-
-class BaseGeometry:
-    """BaseGeometry class"""
-    def integer_validator(self, name, value):
-        """validate if it's an integer or not"""
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-    def __init__(self, width, height):
-        """constructor method"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """return an informal string"""
-        s = f'[Rectangle] {self.__width}/{self.__height}'
-        return s
-
-    def area(self):
-        """area method"""
-        return self.__height * self.__width
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
