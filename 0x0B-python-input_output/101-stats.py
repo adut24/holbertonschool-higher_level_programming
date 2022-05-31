d0923933bc6b2199ffa0cc7b2dc88220b8d34a06
adut24 <dutertre.alexandre@laposte.net>
2022-05-31 19:02:22 +0200

    Correction

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """stats module"""
-import signal
 import sys
 
 
@@ -12,21 +11,18 @@ def print_info(stats, size):
             print(f"{k}: {v}")
 
 
-def signal_handler(sig, frame):
-    """called if Ctrl+C is pressed"""
-    print_info(stats, size)
-
-
 count = 0
 size = 0
 stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
          '405': 0, '500': 0}
 
-signal.signal(signal.SIGINT, signal_handler)
-for line in sys.stdin:
-    count += 1
-    line = line.split()[-2:]
-    size += int(line[1])
-    stats[line[0]] += 1
-    if count % 10 == 0:
-        print_info(stats, size)
+try:
+    for line in sys.stdin:
+        count += 1
+        line = line.split()[-2:]
+        size += int(line[1])
+        stats[line[0]] += 1
+        if count % 10 == 0:
+            print_info(stats, size)
+except KeyboardInterrupt:
+    print_info(stats, size)
