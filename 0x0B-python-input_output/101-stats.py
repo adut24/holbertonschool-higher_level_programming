ac5824f28f2b4cf0a50da3086c4b4b51546c61d6
adut24 <dutertre.alexandre@laposte.net>
2022-05-31 19:22:18 +0200

    Correction

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """stats module"""
+import fileinput
+import signal
 import sys
 
 
 def print_info(stats, size):
     """print the informations"""
-    print(f"File size: {size}")
+    print("File size: {}".format(size))
     for k, v in sorted(stats.items()):
         if v:
-            print(f"{k}: {v}")
+            print("{}: {}".format(k, v))
+
+
+def signal_handler(sig, frame):
+    """called if Ctrl+C is pressed"""
+    print_info(stats, size)
 
 
 count = 0
 size = 0
 stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
          '405': 0, '500': 0}
-try:
-    for line in sys.stdin:
-        count += 1
-        line = line.split()[-2:]
-        size += int(line[1])
-        stats[line[0]] += 1
-        if count % 10 == 0:
-            print_info(stats, size)
-    print_info(stats, size)
-except KeyboardInterrupt:
-    print_info(stats, size)
+
+signal.signal(signal.SIGINT, signal_handler)
+for line in fileinput.input():
+    count += 1
+    line = line.split()[-2:]
+    size += int(line[1])
+    stats[line[0]] += 1
+    if count % 10 == 0:
+        print_info(stats, size)
+print_info(stats, size)
