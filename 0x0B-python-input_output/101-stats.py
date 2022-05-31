9fc4d0219465ae1428f3bc89a1d200fc780910b1
adut24 <dutertre.alexandre@laposte.net>
2022-05-31 19:09:23 +0200

    Correction

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 """stats module"""
+import fileinput
+import signal
 import sys
+
+
+def print_info(stats, size):
+    """print the informations"""
+    print("File size: {}".format(size))
+    for k, v in sorted(stats.items()):
+        if v:
+            print("{}: {}".format(k, v))
+
+
+def signal_handler(sig, frame):
+    """called if Ctrl+C is pressed"""
+    print_info(stats, size)
 
 
 count = 0
@@ -8,20 +23,12 @@ size = 0
 stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
          '405': 0, '500': 0}
 
-try:
-    for line in sys.stdin:
-        count += 1
-        line = line.split()[-2:]
-        if line[0] in stats:
-            stats[line[0]] += 1
-        size += int(line[1])
-        if count % 10 == 0:
-            print(f"File size: {size}")
-            for k, v in sorted(stats.items()):
-                if v:
-                    print(f"{k}: {v}")
-except KeyboardInterrupt:
-    print(f"File size: {size}")
-    for k, v in sorted(stats.items()):
-        if v:
-            print(f"{k}: {v}")
+signal.signal(signal.SIGINT, signal_handler)
+for line in fileinput.input():
+    count += 1
+    line = line.split()[-2:]
+    size += int(line[1])
+    stats[line[0]] += 1
+    if count % 10 == 0:
+        print_info(stats, size)
+print_info(stats, size)
