a194e61f821092fcc6c9f11399a4fbb354eadba4
adut24 <dutertre.alexandre@laposte.net>
2022-06-07 09:56:14 +0200

    Correction

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,10 +22,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """return the JSON string of list_dictionaries"""
-        if not isinstance(list_dictionaries, list):
-            raise TypeError
-        if list_dictionaries is None or list_dictionaries == []:
-            return []
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
