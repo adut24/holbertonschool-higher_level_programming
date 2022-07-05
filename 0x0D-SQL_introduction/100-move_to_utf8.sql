17f92c7255544e6eb46065da1f86cc9afe91fbb8
adut24 <dutertre.alexandre@laposte.net>
2022-07-05 14:37:15 +0200

    Adding advanced tasks

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Convert hbtn_0c_0 to UTF8
+-- Query that converts hbtn_0c_0 to UTF8
+USE hbtn_0c_0;
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
