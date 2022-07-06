c7ffbc82198ae955719ae412e151da5c208524b5
adut24 <dutertre.alexandre@laposte.net>
2022-07-06 12:17:13 +0200

    Correction

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- List all cities contained in the database hbtn_0d_usa
 -- Query that lists all cities contained in the database hbtn_0d_usa
-SELECT *, states.name
+SELECT id, name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id = states.id;
