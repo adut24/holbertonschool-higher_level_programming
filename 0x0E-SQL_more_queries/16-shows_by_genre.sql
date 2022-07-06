13af539fe4b866f9a7d4967e851deef8412d571e
adut24 <dutertre.alexandre@laposte.net>
2022-07-06 14:39:31 +0200

    Add task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all shows, and all genres linked to that show
+-- Query that lists all shows, and all genres linked to that show
+SELECT title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title ASC, tv_genres.name ASC;
