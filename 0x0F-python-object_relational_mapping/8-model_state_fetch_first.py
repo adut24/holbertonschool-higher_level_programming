a1a9d561ac64625c8dc852ec554f9e0cc968f68f
adut24 <dutertre.alexandre@laposte.net>
2022-08-04 17:36:05 +0000

    Correction of docstring

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module listing all states"""
+"""Module listing the first state"""
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
