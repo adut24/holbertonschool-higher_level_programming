e0400f09c5ebc309bef13e1f2d71b5fb491e869e
adut24 <dutertre.alexandre@laposte.net>
2022-08-06 08:38:05 +0000

    Correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;"
                 .format(argv[4]))
     res = cur.fetchall()
-    print(res)
+    for r in res:
+        if r[1] == argv[4]:
+            print(r)
     cur.close()
     db.close()
