db119ac9e9f3b6851bda7b1b02e700356493c8b8
adut24 <dutertre.alexandre@laposte.net>
2022-08-19 11:09:49 +0000

    Correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Take GitHub credentials"""
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     url = ' https://api.github.com/user'
-    response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
-    r = response.json()
+    r = requests.get(url, auth=(argv[1], argv[2]))
+    r = r.json()
     print(r.get('id'))
