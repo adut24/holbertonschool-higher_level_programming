0bac67de42e18363f487da502bac6d1a71311a37
adut24 <dutertre.alexandre@laposte.net>
2022-08-19 11:03:15 +0000

    Correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,6 @@ import sys
 
 if __name__ == "__main__":
     url = ' https://api.github.com/user'
-    session = requests.Session()
-    response = session.get(url, auth=(sys.argv[1], sys.argv[2]))
+    response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     r = response.json()
     print(r.get('id'))
