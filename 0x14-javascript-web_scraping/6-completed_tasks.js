de4f26207103986dd4d6e500acc065693cfec106
adut24 <dutertre.alexandre@laposte.net>
2022-09-13 13:45:30 +0200

    List the number of completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const dict = {};
+axios.get(process.argv[2])
+  .then(function (response) {
+    response.data.forEach(function (task, idx) {
+      if (dict[task.userId] === undefined) {
+        dict[task.userId] = 0;
+      }
+      if (task.completed) {
+        dict[task.userId] += 1;
+      }
+    });
+    console.log(dict);
+  });
