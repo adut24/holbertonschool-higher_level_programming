9ac00fee9af02606db46699f038d911e98e1b66e
adut24 <dutertre.alexandre@laposte.net>
2022-09-13 12:10:05 +0200

    Correction of semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,10 +2,11 @@
 const axios = require('axios').default;
 axios.get(process.argv[2])
   .then(function (response) {
-      let nb = 0;
-      response.data.results.forEach(movie => {
-        if (movie.characters.some(charac => charac.includes('18')))
-          nb++;
-      })
-      console.log(nb);
+    let nb = 0;
+    response.data.results.forEach(movie => {
+      if (movie.characters.some(charac => charac.includes('18'))) {
+        nb++;
+      }
+    });
+    console.log(nb);
   });
